5203d71c0c04068d2f053fb5dd88c5a81ec7cfe9
slmanasad <slman.me@gmail.com>
2017-10-11 23:11:08 +0500

    Initial commit

---
+++ app/src/main/java/com/example/salman/gson_test/Contact.java
@@ -0,0 +1,41 @@
+package com.example.salman.gson_test;
+
+/**
+ * Created by Salman on 10/10/2017.
+ */
+
+public class Contact {
+    private int id;
+    private String name;
+    private boolean gender;
+
+    public Contact(int id, String name, boolean gender) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+}
